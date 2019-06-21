803f0866be258baf9fec16d5616c87639c487d08
Jordan Holmes <jordanstholmes@gmail.com>
2019-06-21 16:12:46 -0700

    Refactor App to class component

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,29 @@
-const movies = [
-  {id:0, title: 'Mean Girls'},
-  {id:1, title: 'Hackers'},
-  {id:2, title: 'The Grey'},
-  {id:3, title: 'Sunshine'},
-  {id:4, title: 'Ex Machina'},
-];
-
 /*
 - add a search bar (and button)
   - figure out input text field
 
 
 - add some way to handle click events
-  - store the text value for the input in React state
-  - when new input is entered, first change it in the React state,
-  then render the component based off that React state.
-
-
+  - setup "Controlled Component"
+    - store the text value for the input in React state
+    - when new input is entered, first change it in the React state,
+    then render the component based off that React state.
 
 - add functionality to handle search
-
-
-
 */
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>MovieList</h1>
+          <input type="text" />
+          <button onClick={() => console.log('Jordan')}>Search</button>
+        <ul>
+          { this.props.movies.map(movie => <li key={ movie.id } >{ movie.title }</li>) }
+        </ul>
+      </div>
+    );
+  }
+}
 
-const App = () => (
-  <div>
-    <h1>MovieList</h1>
-      <input type="text" />
-      <button onClick={() => console.log('Jordan')}>Search</button>
-    <ul>
-      { movies.map(movie => <li key={ movie.id } >{ movie.title }</li>) }
-    </ul>
-  </div>
-);
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,11 @@
 import App from './app.js';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+const movies = [
+  {id:0, title: 'Mean Girls'},
+  {id:1, title: 'Hackers'},
+  {id:2, title: 'The Grey'},
+  {id:3, title: 'Sunshine'},
+  {id:4, title: 'Ex Machina'},
+];
+
+ReactDOM.render(<App movies={ movies }/>, document.getElementById('app'));
