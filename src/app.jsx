1f08280f6e89bd55b61a847d734df282161f89bf
Jordan Holmes <jordanstholmes@gmail.com>
2019-06-21 20:27:20 -0700

    Implement filter

--- src/app.jsx
+++ src/app.jsx
@@ -2,37 +2,47 @@
 - add a search bar (and button)
   - figure out input text field
 
-
-- add some way to handle click events
-  - setup "Controlled Component"
-    - store the text value for the input in React state
-    - when input text changes, update the state.
-    - when new input is entered, first change it in the React state,
-    then render the component based off that React state.
-
 - add functionality to handle search
+  - on search click
+  - compare input text to titles,
+  - filter for movies whose title contains the input text
+  - render our newly filtered list
+
+
+  - new function (pass it the data)
+  - filter the data,
+  - when you click "search" filter the
+  - add a thing to the state for the list of movies being rendered.
 */
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { inputText: '' };
+    this.state = {
+      inputText: '',
+      filterBy: '',
+    };
     this.handleChange = this.handleChange.bind(this);
+    this.searchHandler = this.searchHandler.bind(this);
   }
 
   handleChange(event) {
     const inputText = event.target.value;
     this.setState({ inputText });
-  } // I am trivial
+  }
+
+  searchHandler() {
+    this.setState({ filterBy: this.state.inputText });
+  }
 
   render() {
-    console.log(this.state.inputText);
+    const filteredMovies = this.props.movies.filter(movie => movie.title.includes(this.state.filterBy));
     return (
       <div>
         <h1>MovieList</h1>
           <input value={this.state.inputText} onChange={ this.handleChange } type="text" />
-          <button onClick={() => console.log(this)}>Search</button>
+          <button onClick={this.searchHandler}>Search</button>
         <ul>
-          { this.props.movies.map(movie => <li key={ movie.id } >{ movie.title }</li>) }
+          { filteredMovies.map(movie => <li key={ movie.id } >{ movie.title }</li>) }
         </ul>
       </div>
     );
