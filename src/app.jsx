9054d98dea5691c47c8eb98cdd3a53309d39532f
Jordan Holmes <jordanstholmes@gmail.com>
2019-06-21 17:14:50 -0700

    Implement basic event handler for search
    Good job guys!

--- src/app.jsx
+++ src/app.jsx
@@ -6,18 +6,31 @@
 - add some way to handle click events
   - setup "Controlled Component"
     - store the text value for the input in React state
+    - when input text changes, update the state.
     - when new input is entered, first change it in the React state,
     then render the component based off that React state.
 
 - add functionality to handle search
 */
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputText: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const inputText = event.target.value;
+    this.setState({ inputText });
+  }
+
   render() {
+    console.log(this.state.inputText);
     return (
       <div>
         <h1>MovieList</h1>
-          <input type="text" />
-          <button onClick={() => console.log('Jordan')}>Search</button>
+          <input value={this.state.inputText} onChange={ this.handleChange } type="text" />
+          <button onClick={() => console.log(this)}>Search</button>
         <ul>
           { this.props.movies.map(movie => <li key={ movie.id } >{ movie.title }</li>) }
         </ul>
